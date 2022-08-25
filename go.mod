4f21e3f62f9bcb143d3010198df6fef3ba54198b
bzzz <nweather2@gmail.com>
2022-08-25 15:46:25 -0400

    cha changes

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require github.com/jinzhu/gorm v1.9.16
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/mattn/go-sqlite3 v1.14.9 // indirect
--- lookup.go
+++ lookup.go
@@ -2,9 +2,10 @@ package lookup
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	"strings"
 )
 
 type Student struct {
@@ -16,7 +17,7 @@ type Student struct {
 }
 
 func ConnectToDBFindAll(s string) *gorm.DB {
-	db, err := gorm.Open("sqlite3", "/Users/bzzz/go/github.com/nicksbuggycode/orm/studz.db")
+	db, err := gorm.Open("sqlite3", "/Users/bzzz/go/src/github.com/nicksbuggycode/orm/studz.db")
 	if err != nil {
 		fmt.Println(err.Error())
 		panic("failed to connect database")
@@ -31,7 +32,7 @@ func ConnectToDBFindAll(s string) *gorm.DB {
 }
 
 func ConnectToDBFindSome(s string) *gorm.DB {
-	db, err := gorm.Open("sqlite3", "/Users/bzzz/go/github.com/nicksbuggycode/orm/studz.db")
+	db, err := gorm.Open("sqlite3", "/Users/bzzz/go/src/github.com/nicksbuggycode/orm/studz.db")
 	if err != nil {
 		fmt.Println(err.Error())
 		panic("failed to connect database")
@@ -45,21 +46,7 @@ func ConnectToDBFindSome(s string) *gorm.DB {
 	builder.WriteString("%")
 	db.Where("firstname LIKE ?", builder.String()).Find(&students)
 	for _, stud := range students {
-		if stud.Class == 18 {
-			fmt.Println(stud.Firstname, stud.Lastname, "is in class ", stud.Class)
-		} else {
-			fmt.Println(stud.Firstname, stud.Lastname, "is in class 17")
-		}
-	}
-	if len(students) == 0 {
-		db.Where("lastname LIKE ?", builder.String()).Find(&students)
-		for _, stud := range students {
-			if stud.Class == 18 {
-				fmt.Println(stud.Firstname, stud.Lastname, "is in class ", stud.Class)
-			} else {
-				fmt.Println(stud.Firstname, stud.Lastname, "is in class 17")
-			}
-		}
+		fmt.Println(stud.Firstname, stud.Lastname, "is in class ", stud.Class)
 	}
 	return nil
 }
